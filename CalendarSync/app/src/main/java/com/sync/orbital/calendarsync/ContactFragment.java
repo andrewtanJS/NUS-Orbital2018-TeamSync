2201a4f78775e6e24c1f13988ea7c59707951082
jlang <jieliangang@gmail.com>
2018-06-30 17:41:22 +0800

    Merge and Added Tab Fragments

--- CalendarSync/app/src/main/java/com/sync/orbital/calendarsync/ContactFragment.java
+++ CalendarSync/app/src/main/java/com/sync/orbital/calendarsync/ContactFragment.java
@@ -2,7 +2,10 @@ package com.sync.orbital.calendarsync;
 
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,17 +16,32 @@ import android.view.ViewGroup;
  */
 public class ContactFragment extends Fragment {
 
+    private TabLayout tabLayout;
+    private AppBarLayout appBarLayout;
+    private ViewPager viewPager;
 
     public ContactFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact, container, false);
+        View view = inflater.inflate(R.layout.fragment_contact, container, false);
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewpager_cont);
+
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager());
+        adapter.AddFragment(new ContactFreeFragment(), "Free");
+        adapter.AddFragment(new ContactAllFragment(), "All");
+        adapter.AddFragment(new ContactRequestFragment(), "Request");
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tab_layout_cont);
+        tabLayout.setupWithViewPager(viewPager);
+
+        return view;
     }
 
 }
