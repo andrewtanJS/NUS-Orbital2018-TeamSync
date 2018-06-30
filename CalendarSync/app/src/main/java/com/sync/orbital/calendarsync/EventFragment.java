4c7d2b72e7fd00dc89957c2e08bb32d734e45c8c
U-DESKTOP-UBJ36T9\andre <andrew.jinshen@gmail.com>
2018-06-30 18:26:50 +0800

    Changed calendar api used

--- CalendarSync/app/src/main/java/com/sync/orbital/calendarsync/EventFragment.java
+++ CalendarSync/app/src/main/java/com/sync/orbital/calendarsync/EventFragment.java
@@ -2,7 +2,10 @@ package com.sync.orbital.calendarsync;
 
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,16 +16,31 @@ import android.view.ViewGroup;
  */
 public class EventFragment extends Fragment {
 
+    private TabLayout tabLayout;
+    private AppBarLayout appBarLayout;
+    private ViewPager viewPager;
+
     public EventFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event, container, false);
+        View view = inflater.inflate(R.layout.fragment_event, container, false);
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewpager);
+
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager());
+        adapter.AddFragment(new EventIncomingFragment(), "Incoming");
+        adapter.AddFragment(new EventRequestFragment(), "Requests");
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+
+        return view;
     }
 
 }
