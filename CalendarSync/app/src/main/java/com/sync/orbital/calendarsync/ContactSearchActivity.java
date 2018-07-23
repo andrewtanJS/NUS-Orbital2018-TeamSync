59d7836bbd9584a56770dfc03586f03743324ea6
jlang <jieliangang@gmail.com>
2018-07-23 23:46:34 +0800

    Begin on Search Contacts Activity

---
+++ CalendarSync/app/src/main/java/com/sync/orbital/calendarsync/ContactSearchActivity.java
@@ -0,0 +1,13 @@
+package com.sync.orbital.calendarsync;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContactSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_search);
+    }
+}
