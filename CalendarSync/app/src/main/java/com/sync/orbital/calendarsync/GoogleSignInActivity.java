07121067d4a26febb1fb5e17dfb7dcf3564af57f
U-DESKTOP-UBJ36T9\andre <andrew.jinshen@gmail.com>
2018-07-24 11:25:20 +0800

    Updated datepicker to display current date

---
+++ CalendarSync/app/src/main/java/com/sync/orbital/calendarsync/GoogleSignInActivity.java
@@ -0,0 +1,247 @@
+package com.sync.orbital.calendarsync;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.Events;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class GoogleSignInActivity extends AppCompatActivity {
+
+    private static final String APPLICATION_NAME = "CalendarSync";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String CREDENTIALS_FOLDER = "credentials"; // Directory to store user credentials.
+
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * If modifying these scopes, delete your previously saved credentials/ folder.
+     */
+    private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR_READONLY);
+    private static final String CLIENT_SECRET_DIR = "client_secret.json";
+
+    private Button mGoogleSignInBtn;
+
+    private EditText mUsernameField;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+
+    private String username;
+
+    private static final String TAG = "GoogleActivity";
+    private static final int RC_SIGN_IN = 9001;
+
+    private GoogleSignInClient mGoogleSignInClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_google_sign_in);
+
+        mGoogleSignInBtn = findViewById(R.id.signin_google_btn);
+        mUsernameField = findViewById(R.id.signin_username);
+
+        mGoogleSignInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mUsernameField.getText() != null) {
+                    username = mUsernameField.getText().toString();
+                    signIn();
+                } else {
+                    Toast.makeText(GoogleSignInActivity.this, "Please enter a username", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            //Database Update
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                            String uid = user.getUid();
+
+                            mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+                            HashMap<String, String> userMap = new HashMap<>();
+                            userMap.put("name", username);
+                            userMap.put("status", "Hello World");
+                            userMap.put("image", "default");
+                            userMap.put("thumb_image", "default");
+                            Log.d("GoogleSignIn", "Signed in");
+                            mDatabase.setValue(userMap);
+                            getGoogleCalendarData();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(GoogleSignInActivity.this, "Authentication Failed.", Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                // [START_EXCLUDE]
+                updateUI(null);
+                // [END_EXCLUDE]
+            }
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            Intent mainIntent = new Intent(GoogleSignInActivity.this, MainActivity.class);
+            mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(mainIntent);
+            finish();
+        }
+    }
+
+    private void getGoogleCalendarData() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        //Write message to database
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        // Build a new authorized API client service.
+        try {
+            Toast.makeText(getApplicationContext(),
+                    "Syncing with Google Calendar", Toast.LENGTH_SHORT).show();
+            final NetHttpTransport HTTP_TRANSPORT = new com.google.api.client.http.javanet.NetHttpTransport();
+            com.google.api.services.calendar.Calendar service =
+                    new com.google.api.services.calendar.Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                            .setApplicationName(APPLICATION_NAME)
+                            .build();
+            Calendar cal = Calendar.getInstance();
+            DateTime now = new DateTime(cal.getTimeInMillis());
+            Events googleEvents = service.events().list("primary")
+                    .setTimeMin(now)
+                    .execute();
+            for (Event item : googleEvents.getItems()){
+                Log.d("EventStuff", item.toString());
+                Calendar start = Calendar.getInstance();
+                Calendar end = Calendar.getInstance();
+
+                start.setTimeInMillis(item.getStart().getDateTime().getValue());
+                end.setTimeInMillis(item.getEnd().getDate().getValue());
+
+                EventIncomingStruct eventNew = new EventIncomingStruct(
+                        item.getSummary(),
+                        item.getStatus(),
+                        "0/0",
+                        start,
+                        end
+                );
+                String eventId = mDatabase.push().getKey();
+                mDatabase.child("users").child(user.getUid())
+                        .child("events").child(eventId).setValue(eventNew);
+            }
+
+            Toast.makeText(getApplicationContext(), "Getting Google Calendar Data", Toast.LENGTH_SHORT).show();
+        } catch (Exception IOException) {
+            Toast.makeText(getApplicationContext(), "Error getting data", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Creates an authorized Credential object.
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If there is no client_secret.
+     */
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        // Load client secrets.
+        InputStream in = CalendarFragment.class
+                .getResourceAsStream("client_secret.json");
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(CREDENTIALS_FOLDER)))
+                .setAccessType("offline")
+                .build();
+        return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+    }
+}
